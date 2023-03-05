0d14497048043b2726c560b9957d80cf204fc6f4
eliancode <lea.estel@gmx.de>
2023-03-05 13:25:45 +0100

    work in progress

--- sqlite.js
+++ sqlite.js
@@ -5,22 +5,22 @@ import { addPizza } from "./prisma.js";
 sqlite3.verbose();
 
 async function openDb() {
-  return await open({
-    filename: "./dev.db",
-    driver: sqlite3.cached.Database,
-  });
+    return await open({
+        filename: "./dev.db",
+        driver: sqlite3.cached.Database,
+    });
 }
 
 async function createTable(db) {
-  await db.exec("CREATE TABLE IF NOT EXISTS tbl (col TEXT)");
+    await db.exec("CREATE TABLE IF NOT EXISTS tbl (col TEXT)");
 }
 
 export async function run(req, res) {
-  let db = await openDb();
-  console.log(db);
-  createTable(db);
-  res.send("Ok!");
-  addPizza();
+    let db = await openDb();
+    console.log(db);
+    createTable(db);
+    res.send("Ok!");
+    addPizza();
 }
 
 export default run;
