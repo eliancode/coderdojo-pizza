b635c769f8c232173dd733762833fa900ff1fb2e
sbj <s.bergjohansen@gmail.com>
2023-03-05 19:13:43 +0100

    Reduce parameter number in body-parser

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(
   bodyParser.urlencoded({
     limit: "1mb",
     extended: true,
-    parameterLimit: 200,
+    parameterLimit: 100,
   })
 );
 app.use(express.static(__dirname + "views"));
