90638fec7e233ad870ec157c2250ce713634af7a
eliancode <lea.estel@gmx.de>
2023-03-05 11:08:09 +0100

    work in progress

--- prisma.js
+++ prisma.js
@@ -1,16 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-// export async function addPizza() {
-//   const pizza = await prisma.pizza.create({
-//     data: {
-//       personName: "name",
-//       pizzaName: "pizzaname",
-//       quantity: "quantity",
-//       size: "size",
-//     },
-//   });
-//   console.log(pizza);
-// }
-
-export
+export async function addPizza() {
+  const pizza = await prisma.pizza.create({
+    data: {
+      personName: "name",
+      pizzaName: "pizzaname",
+      quantity: "quantity",
+      size: "size",
+    },
+  });
+  console.log(pizza);
+}
