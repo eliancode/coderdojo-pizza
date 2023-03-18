be4b816825a6a68acf67e6e3702a0f811dd8631b
eliancode <lea.estel@gmx.de>
2023-03-18 15:00:10 +0100

    work in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import  Button  from "@mui/material/Button";
+import { styled } from '@mui/material/styles';
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+import { Router } from "@mui/icons-material";
+let id = 0
+function returnId(){
+    id++
+    return id;
+}
+const rows: GridRowsProp = [
+  { id: returnId(), col1: '', col2: '' },
+  { id: returnId(), col1: '', col2: '' },
+  { id: returnId(), col1: '', col2: '' },
+];
 
-function App() {
+const columns: GridColDef[] = [
+  { field: 'name', headerName: 'Name', width: 150 },
+  { field: 'pizza', headerName: 'Pizza', width: 150 },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: 300, width: '50%' }}>
+      <DataGrid rows={rows} columns={columns} />
     </div>
   );
 }
-
-export default App;
